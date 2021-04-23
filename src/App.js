8b201de5931971cd7e23eed586f89e2e52b065ba
banurekhaMohan279 <banurekha279@gmail.com>
2021-04-23 19:25:34 +0530

    Add home page as a separate component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
-import carousel1 from './img/carousel-1.jpg';
-import carousel2 from './img/carousel-2.jpeg';
-import carousel3 from './img/carousel-3.jpg';
+import Home from './Home';
 import About from './About';
 
 function App() {
@@ -26,46 +24,7 @@ function App() {
 
     <main>
       <div className = "Home container mb-5" id = "home">
-        <div id="carouselExampleDark" className="carousel carousel-dark slide" data-bs-ride="carousel">
-            <div className="carousel-indicators">
-              <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
-              <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
-              <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
-            </div>
-            <div className="carousel-inner">
-              <div className="carousel-item active" data-bs-interval="100000">
-                <img src= {carousel1} className="d-block w-100" alt="carousel img 1"/>
-                <div className="carousel-caption d-none d-md-block">
-                  <h5>First slide label</h5>
-                  <p>Some representative placeholder content for the first slide.</p>
-                </div>
-              </div>
-              <div className="carousel-item" data-bs-interval="2000000">
-                <img src={carousel2} className="d-block w-100" alt="carousel img 2"/>
-                <div className="carousel-caption d-none d-md-block">
-                  <h5>Second slide label</h5>
-                  <p>Some representative placeholder content for the second slide.</p>
-                </div>
-              </div>
-              <div className="carousel-item">
-                <img src={carousel3
-
-                } className="d-block w-100" alt="carousel img 3"/>
-                <div className="carousel-caption d-none d-md-block">
-                  <h5>Third slide label</h5>
-                  <p>Some representative placeholder content for the third slide.</p>
-                </div>
-              </div>
-            </div>
-            <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
-              <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-              <span className="visually-hidden">Previous</span>
-            </button>
-            <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
-              <span className="carousel-control-next-icon" aria-hidden="true"></span>
-              <span className="visually-hidden">Next</span>
-            </button>
-          </div>
+      <Home/>
       </div>
       <div className = "About container mb-5" id = "about">
         <About/>
