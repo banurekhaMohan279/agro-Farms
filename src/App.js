63aeba77112e12bda2f786b8caff6bef9a4c6296
banurekhaMohan279 <banurekha279@gmail.com>
2021-04-23 19:27:01 +0530

    Add Header as new component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Header from './Header';
 import Home from './Home';
 import About from './About';
 
@@ -5,23 +6,8 @@ function App() {
   return (
     <div className="App">
     <header>
-      <nav className="navbar navbar-expand-lg navbar-light">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#">Agro Farms</a>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
-            <div className="navbar-nav">
-              <a className="nav-link active" aria-current="page" href="#home">Home</a>
-              <a className="nav-link" href="#about">About</a>
-              <a className="nav-link" href="#contact">Contact</a>
-            </div>
-          </div>
-        </div>
-      </nav>
+      <Header/>
     </header>
-
     <main>
       <div className = "Home container mb-5" id = "home">
       <Home/>
