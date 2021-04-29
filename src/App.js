9a9d6b4c22003f397a6428fec3e976875daceb2c
banurekhaMohan279 <banurekha279@gmail.com>
2021-04-29 21:34:24 +0530

    Add contact form

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Header from './Header';
 import Home from './Home';
 import About from './About';
+import Contact from './Contact';
 
 function App() {
   return (
@@ -8,16 +9,16 @@ function App() {
       <header>
         <Header/>
       </header>
-      
+
       <main>
         <div className = "Home container mb-5" id = "home">
-        <Home/>
+          <Home/>
         </div>
         <div className = "About container mb-5" id = "about">
           <About/>
         </div>
-        <div className = "Contact" id = "contact">
-          Contact
+        <div className = "Contact container mb-5" id = "contact">
+          <Contact/>
         </div>
       </main>
 
