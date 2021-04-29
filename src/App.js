18a77f3d5dcb7ad9dabbc7d3cb3b94b11d3cacf6
banurekhaMohan279 <banurekha279@gmail.com>
2021-04-29 21:44:28 +0530

    Add footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Header from './Header';
 import Home from './Home';
 import About from './About';
 import Contact from './Contact';
+import Footer from './Footer';
 
 function App() {
   return (
@@ -23,6 +24,7 @@ function App() {
       </main>
 
       <footer>
+        <Footer/>
       </footer>
     </div>
   );
